89224e8043d22452cfdb7d16c0379434fab50f7e
Rebollos, John Raul <john.rebollos@nordicsemi.no>
2024-01-30 13:58:40 +0100

    added init file

---
+++ csvplotter/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+# Import submodules to make them accessible when importing the package
+from .csvplotter import plot
+
+# Define any package-level variables or constants
+VERSION = '0.1'
+
+# Perform any initialization tasks, if necessary
+print("my_module initialized!")
